CodSpeedHQ/codspeed

feat: add experimental --exclude-allocations setting
Add an experimental `--exclude-allocations` flag (env
`CODSPEED_EXCLUDE_ALLOCATIONS`) that signals the backend to exclude
memory allocation time from simulation results.

The value is surfaced as `runner.excludeAllocations` in the upload
metadata so the backend can gate comparability and history on it: runs
with different values are not comparable. The field is additive, so the
metadata version stays at 10 and older payloads remain compatible.

COD-3016

src/cli/exec/mod.rs
@@ -90,6 +90,7 @@ fn build_orchestrator_config(
         extra_env: HashMap::new(),
         fair_sched: args.shared.experimental.experimental_fair_sched,
         cycle_estimation: args.shared.experimental.cycle_estimation,
+        exclude_allocations: args.shared.experimental.exclude_allocations,
     })
 }
 

xtqqczze/CodSpeedHQ-codspeed

src/cli/exec/mod.rs
@@ -90,6 +90,7 @@ fn build_orchestrator_config(
         extra_env: HashMap::new(),
         fair_sched: args.shared.experimental.experimental_fair_sched,
         cycle_estimation: args.shared.experimental.cycle_estimation,
+        exclude_allocations: args.shared.experimental.experimental_exclude_allocations,
     })
 }
 